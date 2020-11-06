a273a0247746f4703b3fdd02511a376d96dfb8c9
justin <justin@arcran.com>
2020-11-06 09:43:58 +0800

    加入vue-router，並把HelloWorld component 改使用 router-view 載入

---
+++ DbTools/clientapp/src/router/index.js
@@ -0,0 +1,18 @@
+﻿import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HelloWorld from '../components/HelloWorld.vue'
+Vue.use(VueRouter)
+export default new VueRouter({
+    routes: [
+        {
+            path: '*',
+            name: 'HelloWorld',
+            component: HelloWorld
+        }, 
+        {
+            path: '/HelloWorld',
+            name: 'HelloWorld',
+            component: HelloWorld
+        }, 
+    ]
+})
