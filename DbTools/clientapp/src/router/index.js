6fd85a41c4e1e70fad3463cf28c5671769068cfd
justin <justin@arcran.com>
2020-11-09 15:22:43 +0800

    加入bootstrap

--- DbTools/clientapp/src/router/index.js
+++ DbTools/clientapp/src/router/index.js
@@ -1,18 +1,24 @@
 ﻿import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
+import TablesToExcel from '../components/TablesToExcel.vue'
 Vue.use(VueRouter)
 export default new VueRouter({
-    routes: [
-        {
+    routes: [{
             path: '*',
             name: 'HelloWorld',
             component: HelloWorld
-        }, 
+        },
         {
             path: '/HelloWorld',
             name: 'HelloWorld',
             component: HelloWorld
-        }, 
+        },
+        {
+            path: '/TablesToExcel',
+            name: 'TablesToExcel',
+            component: TablesToExcel,
+            props: true
+        },
     ]
 })
